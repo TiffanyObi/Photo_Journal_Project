cc68dafae8efbe7fd346f5d86e5aa9d5957d02a6
Tiffany Obi <tiffanyobi123@gmail.com>
2020-01-23 18:25:05 -0500

    Initial Commit

---
+++ Photo_Journal_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo_Journal_Project
+//
+//  Created by Tiffany Obi on 1/23/20.
+//  Copyright © 2020 Tiffany Obi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Photo_Journal_ProjectTests/Photo_Journal_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Photo_Journal_ProjectTests.swift
+//  Photo_Journal_ProjectTests
+//
+//  Created by Tiffany Obi on 1/23/20.
+//  Copyright © 2020 Tiffany Obi. All rights reserved.
+//
+
+import XCTest
+@testable import Photo_Journal_Project
+
+class Photo_Journal_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
